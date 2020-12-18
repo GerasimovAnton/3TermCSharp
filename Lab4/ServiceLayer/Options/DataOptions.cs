23fe289bdcc6f8f2bf5ba58c5baddc1542869e67
Anton-Gerasimov <55298939+Anton-Gerasimov@users.noreply.github.com>
2020-12-18 15:45:34 +0300

    Create DataOptions.cs

---
+++ Lab4/ServiceLayer/Options/DataOptions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using DAL;
+using ConfigurationManager;
+
+
+namespace ServiceLayer
+{
+    public class DataOptions : Options
+    {
+        public string TargetDirectory { get; set; }
+        public ConnectionOptions LogOptions { get; set; } 
+
+        public DataOptions()
+        {
+            string td = AppDomain.CurrentDomain.BaseDirectory + "\\TargetDirectory";
+
+            if (!Directory.Exists(td)) Directory.CreateDirectory(td);
+
+            TargetDirectory = td;
+
+            //---------------------
+
+            LogOptions = new ConnectionOptions();
+            LogOptions.Database = "Log";
+        }
+    }
+}
